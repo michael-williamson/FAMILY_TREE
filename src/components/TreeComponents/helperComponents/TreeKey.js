50f8a13b3be388fe44387ae7dcd85fc94a7c4438
michael-williamson <michaeldavid1721@yahoo.com>
2020-08-25 11:10:08 -0500

    changed classes from stackable

--- src/components/TreeComponents/helperComponents/TreeKey.js
+++ src/components/TreeComponents/helperComponents/TreeKey.js
@@ -10,52 +10,58 @@ export const TreeKey = (props) => {
         </div>
         <div className="ui list">
         <div className="item">
-            <div className="ui two column stackable padded grid" id="keyGenerationColors">
+            <div className="ui two column padded grid" id="keyGenerationColors">
                 <div className={`item eight wide column`} id="itemLabelMine">
                     <h4 className="ui blue header">
                         Generation Colors
                     </h4>
                 </div>
-                <div className="ui eight wide column stackable relaxed grid" id="colorSelectorMine">
-                    <div className={`item ${props.colorArray[0]} column`}>
-                    1st
-                    </div>
-                    <div className={`item ${props.colorArray[1]} column`}>
-                    2nd
-                    </div>
-                    <div className={`item ${props.colorArray[2]} column`}>
-                    3rd
-                    </div>
-                    <div className={`item ${props.colorArray[3]} column`}>
-                    4th
-                    </div>
-                    <div className={`item ${props.colorArray[4]} column`}>
-                    5th
+                <div className="ui eight wide column relaxed grid" id="colorSelectorMine">
+                    <div className="row">
+                        <div className={`item ${props.colorArray[0]} column`}>
+                        1st
+                        </div>
+                        <div className={`item ${props.colorArray[1]} column`}>
+                        2nd
+                        </div>
+                        <div className={`item ${props.colorArray[2]} column`}>
+                        3rd
+                        </div>
+                        <div className={`item ${props.colorArray[3]} column`}>
+                        4th
+                        </div>
+                        <div className={`item ${props.colorArray[4]} column`}>
+                        5th
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-        {/* <div className="item">
-        2
-        </div>
         <div className="item">
-        3
-        </div> */}
-            <div className="ui two column stackable padded grid" id="keyMinimizer">
-                <div className={`item eight wide column`} id="itemLabelMine">
+            <div className="ui two column padded grid" id="keyMinimizer">
+                <div className="item eight wide column" id="itemLabelMine">
                     <h4 className="ui blue header">
                         Show/Hide Children
                     </h4>
                 </div>
-                <div className={`item eight wide column`} id="minimizerIcons">
-                {/* <div className="ui eight wide column stackable relaxed grid" id="colorSelectorMine"> */}
-                    <div className={`item eight wide column`}>
+                <div className="ui eight wide mobile four wide tablet column relaxed grid" id="minimizerIcons">
+                    <div className="item eight wide column">
                         <i class="plus purple icon"></i>
                     </div>
-                    <div className={`item eight wide column`}>
+                    <div className="item eight wide column">
                         <i class="minus purple icon"></i>
                     </div>
-                {/* </div> */}
+                </div>
+            </div>
+            </div>
+            <div className="item">
+                <div className="ui two column centered padded mobile only grid">
+                    <div className="centered row">
+                            <i class="arrow circle down huge blue icon"></i>
+                    </div>
+                    <div className="centered row">
+                        <h4>Scroll Down for Tree</h4>
+                    </div>
                 </div>
             </div>
         </div>
