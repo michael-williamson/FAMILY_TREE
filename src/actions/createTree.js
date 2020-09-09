4e83e7c5e9cdda63b0f51a7125c6b85c79d26278
michael-williamson <michaeldavid1721@yahoo.com>
2020-09-08 20:59:42 -0500

    saved for format

--- src/actions/createTree.js
+++ src/actions/createTree.js
@@ -1,58 +1,59 @@
-import {CREATE_TREE} from './types';
-import history from '../history';
+import { CREATE_TREE } from "./types";
+import history from "../history";
 
 export const createTree = (formValues) => {
+  let objArr = [];
 
-
-let objArr = [];
-
-class ObjConstructor{
-    constructor(name,spouseName,children,generation){
-        this.name = name;
-        this.spouseName = spouseName; 
-        this.children = children;
-        this.generation = generation; 
+  class ObjConstructor {
+    constructor(name, spouseName, children, generation) {
+      this.name = name;
+      this.spouseName = spouseName;
+      this.children = children;
+      this.generation = generation;
     }
-}
+  }
 
-const mappingFn = () => {
+  const mappingFn = () => {
     //start off by hard coding in values to objArr
-    objArr.push({ancestor:formValues[`ancestor`]});  
-    objArr[0].spouseName =formValues[`ancestorspouse`];
+    objArr.push({ ancestor: formValues[`ancestor`] });
+    objArr[0].spouseName = formValues[`ancestorspouse`];
 
-     const arrIterator = (iterObj,generationArg) => {
+    const arrIterator = (iterObj, generationArg) => {
+      if (!iterObj) return;
+      return iterObj.map((element) => {
+        let currentGeneration = generationArg;
+        let generationCounter = generationArg;
+        let name = element[`field`];
+        let spouseName = element[`spouse`] || undefined;
+        let children = element[`arr`] || undefined;
 
-           if(!iterObj) return; 
-           return iterObj.map((element)=>{
-            let currentGeneration = generationArg; 
-            let generationCounter = generationArg; 
-            let name = element[`field`];
-            let spouseName = element[`spouse`]  || undefined; 
-            let children = element[`arr`] || undefined; 
+        //if there are children that will be the next generation
+        if (children) generationCounter += 1;
 
-            //if there are children that will be the next generation
-            if (children)  generationCounter+=1 
-
-                return (!children ? 
-                        new ObjConstructor(name,spouseName,children,currentGeneration) : 
-                        new ObjConstructor(name,spouseName,arrIterator(children,generationCounter),currentGeneration));
-        });//////  
-    }
+        return !children
+          ? new ObjConstructor(name, spouseName, children, currentGeneration)
+          : new ObjConstructor(
+              name,
+              spouseName,
+              arrIterator(children, generationCounter),
+              currentGeneration
+            );
+      }); //////
+    };
     //passing in what would be ancestorChildren named array on formValues;  also starting generation count at 1 since ancestor children are first generation
-    objArr[0].children = arrIterator(formValues[`ancestorChildren`],1); 
-  
-  //adding generation prop and value after objArr was created and all iterations are done
-  objArr[0].generation =0;
-  return objArr[0]
-} 
+    objArr[0].children = arrIterator(formValues[`ancestorChildren`], 1);
 
-let newObj = mappingFn();
+    //adding generation prop and value after objArr was created and all iterations are done
+    objArr[0].generation = 0;
+    return objArr[0];
+  };
 
+  let newObj = mappingFn();
 
-history.push(`${process.env.PUBLIC_URL}/tree`);
+  history.push(`${process.env.PUBLIC_URL}/tree`);
 
-return {
+  return {
     type: CREATE_TREE,
-    payload: newObj
-}
-}
+    payload: newObj,
+  };
+};
--- src/actions/getSpouseState.js
+++ src/actions/getSpouseState.js
@@ -1,25 +1,25 @@
-import {GET_SPOUSE_STATE} from './types'
+import { GET_SPOUSE_STATE } from "./types";
 
-export const getSpouseState = (bool,name,prevState) => {
-    function objConstructor(bool,name){
-        this.bool = bool;
-        this.name = name;
-    }
+export const getSpouseState = (bool, name, prevState) => {
+  function objConstructor(bool, name) {
+    this.bool = bool;
+    this.name = name;
+  }
 
-    let prevStateIterator = () => {
-        for(let i = 0; i < prevState.length; i++){
-            if(prevState[i].name === name){
-                prevState[i].bool = bool;
-                return prevState; 
-            }
-        }
-        prevState.push(new objConstructor(bool,name));
-        return prevState; 
+  let prevStateIterator = () => {
+    for (let i = 0; i < prevState.length; i++) {
+      if (prevState[i].name === name) {
+        prevState[i].bool = bool;
+        return prevState;
+      }
     }
+    prevState.push(new objConstructor(bool, name));
+    return prevState;
+  };
 
-    let arr = prevStateIterator()
-    return {
-        type: GET_SPOUSE_STATE,
-        payload: arr
-    }
-}
+  let arr = prevStateIterator();
+  return {
+    type: GET_SPOUSE_STATE,
+    payload: arr,
+  };
+};
