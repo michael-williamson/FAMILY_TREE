6e6511096343623a2939e85e68a97545d6e6036e
michael-williamson <michaeldavid1721@yahoo.com>
2020-09-30 21:34:14 -0500

    minor formatting

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-import {Router,Route, Switch} from 'react-router-dom';
-import history from './history';
+import { Router, Route, Switch } from "react-router-dom";
+import history from "./history";
 
-import Tree from './components/TreeComponents/Tree';
-import AncestorEnter from './components/FormComponents/AncestorEnter';
-import {NoMatch} from './components/NoMatch';
+import Tree from "./components/TreeComponents/Tree";
+import AncestorEnter from "./components/FormComponents/AncestorEnter";
+import { NoMatch } from "./components/NoMatch";
 
 function App() {
   return (
     <div className="App">
       <Router history={history} basename={`${process.env.PUBLIC_URL}/`}>
         <Switch>
-          <Route exact path={`${process.env.PUBLIC_URL}/`} render={(routeProps)=><AncestorEnter {...routeProps}/>}/>
-          <Route exact path={`${process.env.PUBLIC_URL}/tree`} render={()=><Tree/>}/>
+          <Route
+            exact
+            path={`${process.env.PUBLIC_URL}/`}
+            render={(routeProps) => <AncestorEnter {...routeProps} />}
+          />
+          <Route
+            exact
+            path={`${process.env.PUBLIC_URL}/tree`}
+            render={() => <Tree />}
+          />
           <Route>
-              <NoMatch/>
+            <NoMatch />
           </Route>
         </Switch>
       </Router>
