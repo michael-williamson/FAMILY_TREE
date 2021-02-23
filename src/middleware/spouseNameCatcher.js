9c0f09a80e6394712ea920d5a797b7e3f735a8fc
michael-williamson <michaeldavid1721@yahoo.com>
2021-02-23 16:54:58 -0600

    removed console.log from code

--- src/middleware/spouseNameCatcher.js
+++ src/middleware/spouseNameCatcher.js
@@ -1,32 +1,30 @@
-import {GET_SPOUSE_STATE} from '../actions/types'
+import { GET_SPOUSE_STATE } from "../actions/types";
 
-export default function spouseNameCatcher ({dispatch,getState}) {
-    return  next => action => {
-        if(action.type === '@@redux-form/UNREGISTER_FIELD') {
-            if (action.payload.name.indexOf('isSpouse') > -1) {
-                let state = getState()
-                let {spouseState} = state; 
-                console.log(spouseState,"spouseState");
-                let prevStateIterator = () => {
-                    for(let i = 0; i < spouseState.length; i++){
-                        if(spouseState[i].name === action.payload.name){
-                            spouseState[i].bool = false;
-                            return spouseState; 
-                        }
-                    }
-                    return spouseState;
-                }
-            
-                let arr = prevStateIterator()
-                console.log(arr,"array spouse state")
-
-                dispatch({
-                    type: GET_SPOUSE_STATE,
-                    payload: arr
-                })
+export default function spouseNameCatcher({ dispatch, getState }) {
+  return (next) => (action) => {
+    if (action.type === "@@redux-form/UNREGISTER_FIELD") {
+      if (action.payload.name.indexOf("isSpouse") > -1) {
+        let state = getState();
+        let { spouseState } = state;
+        let prevStateIterator = () => {
+          for (let i = 0; i < spouseState.length; i++) {
+            if (spouseState[i].name === action.payload.name) {
+              spouseState[i].bool = false;
+              return spouseState;
             }
-        } 
+          }
+          return spouseState;
+        };
 
-        next(action)
+        let arr = prevStateIterator();
+
+        dispatch({
+          type: GET_SPOUSE_STATE,
+          payload: arr,
+        });
+      }
     }
+
+    next(action);
+  };
 }
